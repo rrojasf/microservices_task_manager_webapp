bae55d8414f7b1311d5b2478e8affdbc88af813b
Robert Rojas Fernandez <rrojasf@>
2019-11-05 01:50:08 -0500

    set default api references for online services

--- src/config.js
+++ src/config.js
@@ -1,7 +1,5 @@
-const API_USERS_URL = "http://localhost:81/api/users/"
-const API_TASKS_URL = "http://localhost:82/api/tasks/"
-const API_USERS_URL_1 = "https://taskmgr-ms-user-api.herokuapp.com/api/users/"
-const API_TASKS_URL_2 = "https://taskmgr-ms-task-api.herokuapp.com/api/tasks/"
+const API_USERS_URL = "https://taskmgr-ms-user-api.herokuapp.com/api/users/"
+const API_TASKS_URL = "https://taskmgr-ms-task-api.herokuapp.com/api/tasks/"
 const PAGE_OFFSET = 20
 const PAGE_LIMIT = 20
 
---
+++ src/config.local.js
@@ -0,0 +1,6 @@
+const API_USERS_URL = "http://localhost:81/api/users/"
+const API_TASKS_URL = "http://localhost:82/api/tasks/"
+const PAGE_OFFSET = 20
+const PAGE_LIMIT = 20
+
+export { API_USERS_URL, API_TASKS_URL, PAGE_OFFSET, PAGE_LIMIT }
